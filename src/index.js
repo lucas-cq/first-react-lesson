1a9f58e476f1a8bc1a9fdc3163fee2f38d8a343f
lucas-cq <lucas.cummings024@gmail.com>
2022-01-30 17:10:45 -0700

    update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import ReactDom from 'react-dom'
 
-function Header() {
-  return <h1>Hello World</h1>
+// Use "className=" instead of "class="
+
+const Footer = () => {
+  return <h1>Hello World</h1> //Opening Tags MUST be on the same line as return, or you can use return ()
 }
+
+ReactDom.render(<Footer/>,document.getElementById('root'))
